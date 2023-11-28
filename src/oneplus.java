49abc1d7e62a465b56dab231b8616203a2069617
Prashant-apnacollege <prashantmali381@gmail.com>
2023-11-28 17:03:05 +0530

    This is my Second Commit

---
+++ src/oneplus.java
@@ -0,0 +1,15 @@
+
+public class oneplus {
+	
+	public void method() {
+		System.out.println("This is my first method");
+	}
+
+	public static void main(String [] args) {
+		
+		
+		   oneplus o = new oneplus();
+		   o.method();
+		}
+	
+}
